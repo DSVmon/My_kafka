97aa12ef85e4755b8d05cf958a56cfa5d4462ab7
Dmitry <DSVmail@tut.by>
2025-04-14 22:01:59 +0300

    v1

--- app/main.py
+++ app/main.py
@@ -1,21 +1,21 @@
 from kafka import KafkaProducer, KafkaConsumer
 import time
 def produce_message():
-    producer=KafkaProducer(bootstrap_server='localhost:9092')
+    producer=KafkaProducer(bootstrap_servers='localhost:9092')
     message=b'Hello, KAFKA!'
-    producer.send('test-topic',message)
+    producer.send('test-topic', message)
     producer.flush()
-    print('Сообщение отправлено: ' + message)
+    print('Сообщение отправлено: ', message)
 
 def consume_message():
     consumer=KafkaConsumer(
         'test-topic', 
-        bootstrap_server='localhost:9092', 
+        bootstrap_servers='localhost:9092', 
         auto_offset_reset='earliest', 
         consumer_timeout_ms=5000)
     
     for message in consumer:
-        print('Сообщение получено: ' + str(message.value))
+        print('Сообщение получено: ', str(message.value))
         time.sleep(1)
 
 if __name__=='main':
